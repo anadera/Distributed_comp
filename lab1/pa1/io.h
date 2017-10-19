d36cc103574513d8affbf3d8a73f378ed5f90180
anadera <anadera@users.noreply.github.com>
2017-10-19 20:08:53 +0300

    Update io.h

--- lab1/pa1/io.h
+++ lab1/pa1/io.h
@@ -5,6 +5,7 @@
 #include <stdlib.h> //atoi exit
 #include <string.h> //strcpy strcmp strlen
 #include <stdio.h> //perror printf fprintf
+#include <time.h> //time
 
 int parse_x(char** argv);
 void create_msg(Message msg, MessageType type, const char * const body);
