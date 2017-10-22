0d6b0fcebd3903e64a42ecba072aecd96bea6d41
Анжелика Дерябина <hey_biscuit@mail.ru>
2017-10-22 23:10:07 +0300

    f

--- lab1/pa2/operations.h
+++ lab1/pa2/operations.h
@@ -3,8 +3,6 @@
 #include "lab2.h"
 #include "banking.h"
 #include "ipc.h"
-#include "io.h"
-#include "pa2345.h"
 
 void set_start_balance(local_id self, BalanceHistory* h, int* array);
 void set_balance(BalanceHistory* history, balance_t amount);
