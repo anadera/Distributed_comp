436b7cd2ff9631f3ad0363b8ff1cfe1a4e3d206a
Анжелика Дерябина <hey_biscuit@mail.ru>
2017-11-26 21:16:36 +0300

    output -r

---
+++ lab1/pa1.tar/pa1/pa1/io.c
@@ -0,0 +1,65 @@
+#include "io.h"
+#include "ipc.h"
+#include "pa1.h"
+
+/*
+parse command line arguments
+return X from "./lab2 -p X"
+*/
+int parse_x(char** argv){
+	int d;
+	if(!strcmp(argv[1], "-p")){
+		if ((d = atoi(argv[2]))>0 && (d = atoi(argv[2])) <=10)
+			return d;
+		else {
+			perror("parse_x:wrong X");
+			exit(EXIT_FAILURE);
+		}
+
+	}
+	else {
+		perror("parse_x:wrong key");
+		exit(EXIT_FAILURE);
+  }
+}
+
+/*
+fill declared Message struct
+*/
+void create_msg(Message msg, MessageType type, const char * const body, int id){
+	char tmp[MAX_PAYLOAD_LEN] = "";
+	size_t buf = 0;
+	if (body == log_started_fmt){
+			buf = sprintf(tmp, body, id, getpid(), getppid());
+	}
+	else {
+			buf = sprintf(tmp, body, id);
+	}
+	msg.s_header = (MessageHeader) {
+		.s_magic = MESSAGE_MAGIC,
+		.s_payload_len = buf,
+		.s_type = type,
+		.s_local_time = time(0)
+	};
+	strncpy(msg.s_payload, tmp, buf);
+}
+
+/*
+log event to stdout and in file
+fmt - event_message
+self - local process id
+des - descriptor of opened file
+*/
+void log_events(const char * const fmt, int self, FILE* des){
+	if (printf(fmt,self,getpid(),getppid()) < 0)
+		perror("log_events:printf");
+	if (fprintf((FILE*)des,fmt,self,getpid(),getppid()) < 0)
+		perror("log_events:fprintf");
+}
+
+void log_pipes(const char * const fmt, int self, int fd_R, int fd_W, FILE* des){
+	if (printf(fmt,self,getpid(),getppid(), fd_R,fd_W) < 0)
+		perror("log_events:printf");
+	if (fprintf((FILE*)des,fmt,self,getpid(),getppid(), fd_R, fd_W) < 0)
+		perror("log_events:fprintf");
+}
---
+++ lab1/pa2.tar/pa2/pa2/ipc.c
@@ -0,0 +1,110 @@
+#include "ipc.h"
+#include "lab2.h"
+
+/** Send a message to the process specified by id.
+ *
+ * @param self    Any data structure implemented by students to perform I/O
+ * @param dst     ID of recepient
+ * @param msg     Message to send
+ *
+ * @return 0 on success, any non-zero value on error
+ */
+
+ const int block = 8192; //64 Kbit
+
+int send(void * self, local_id dst, const Message * msg){
+	PROCESS *p = (PROCESS*)self;
+  int des;
+	//int count = strlen(msg);
+  if (p->id == dst){
+    printf("send:src=dst id=%d dst=%d msg=%d\n",p->id, dst, msg->s_header.s_type);
+    return FAILURE;
+  }
+	des = p->fd[dst][1];
+  printf("%d: process id=%d send msg type=%d to dst=%d\n", get_physical_time(), p->id, msg->s_header.s_type, des);
+  size_t size = sizeof(msg->s_header) + msg->s_header.s_payload_len;
+	int status = write(des, msg, size);
+  return status > 0 ? SUCCESS : FAILURE;
+}
+
+/** Send multicast message.
+ *
+ * Send msg to all other processes including parrent.
+ * Should stop on the first error.
+ *
+ * @param self    Any data structure implemented by students to perform I/O
+ * @param msg     Message to multicast.
+ *
+ * @return 0 on success, any non-zero value on error
+ */
+int send_multicast(void * self, const Message * msg){
+	int i;
+	PROCESS *p = (PROCESS*)self;
+	//int count = strlen(msg);
+	int size = p->x;
+	for (i=0; i<=size; i++){
+		send((void*)p,i,msg);
+	}
+	return SUCCESS;
+}
+
+/** Receive a message from the process specified by id.
+ *
+ * Might block depending on IPC settings.
+ *
+ * @param self    Any data structure implemented by students to perform I/O
+ * @param from    ID of the process to receive message from
+ * @param msg     Message structure allocated by the caller
+ *
+ * @return 0 on success, any non-zero value on error
+ */
+
+int receive(void * self, local_id from, Message * msg){
+	PROCESS *p = (PROCESS*)self;
+  if (p->id == from)
+    return FAILURE;
+	int fd = p->fd[from][0];
+	int read_bytes = read(fd, msg, sizeof(Message));
+	if (read_bytes < 0 && errno != EAGAIN) {
+        perror("receive:read");
+        return FAILURE;
+  }
+  return read_bytes > 0 ? SUCCESS : FAILURE;
+}
+
+/** Receive a message from any process.
+ *
+ * Receive a message from any process, in case of blocking I/O should be used
+ * with extra care to avoid deadlocks.
+ *
+ * @param self    Any data structure implemented by students to perform I/O
+ * @param msg     Message structure allocated by the caller
+ *
+ * @return 0 on success, any non-zero value on error
+ */
+int receive_any(void * self, Message * msg){
+	PROCESS *p = (PROCESS*)self;
+  char buff[MAX_MESSAGE_LEN];
+	int size = p->x;
+  int des = 0;
+  int read_bytes = 0;
+  int i;
+  while(1){
+    for (i=0; i<=size; i++){
+      //printf("receive_any: i=%d id=%d\n", i, p->id);
+      des=p->fd[i][0];
+      if (des == 0)
+        continue;
+      //printf("receive_any: des=%d id=%d\n", des, p->id);
+      read_bytes = read(des,buff, MAX_MESSAGE_LEN);
+      if (read_bytes>0){
+        memcpy(msg,buff,read_bytes);
+        return SUCCESS;
+      }
+      else
+        continue;
+    }
+    usleep(10000);
+  }
+  return FAILURE;
+}
