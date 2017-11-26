cd991359998bf26189038897d2904ba1d7f37ae6
Anzhelika Deryabina <anzhelika.deryabina@balvenie.orcsoftware.com>
2017-11-26 21:56:59 +0100

    fix time log

--- lab1/pa2/lab2.c
+++ lab1/pa2/lab2.c
@@ -186,6 +186,7 @@ int child_work(PROCESS* p, BalanceHistory* h){
 	memset(&msgBH, 0, sizeof(msgBH));
 	balance_t fin_balance;
 	int status;
+	int time;
 	char tmp[MAX_PAYLOAD_LEN] = "";
 	//printf("start child_work\n");
 	while (1){
@@ -205,34 +206,30 @@ int child_work(PROCESS* p, BalanceHistory* h){
 					//fprintf(p->events,log_transfer_in_fmt,get_physical_time(),p->id,order.s_amount,0);
 					//printf(log_transfer_in_fmt,get_physical_time(),p->id,order.s_amount,0);
 					set_balance(h, -(order.s_amount));
-					msg.s_header.s_local_time = get_physical_time();
+					//msg.s_header.s_local_time = get_physical_time();
 					if (send(p,order.s_dst,(const Message *)&msg) != 0){
 						perror("send TRANSFER is failed");
 						exit(EXIT_FAILURE);
 					}
-					fprintf(p->events,log_transfer_out_fmt,get_physical_time(),p->id,order.s_amount,order.s_dst);
-					printf(log_transfer_out_fmt,get_physical_time(),p->id,order.s_amount,order.s_dst);
+					fprintf(p->events,log_transfer_out_fmt, msg.s_header.s_local_time,p->id,order.s_amount,order.s_dst);
+					printf(log_transfer_out_fmt,msg.s_header.s_local_time,p->id,order.s_amount,order.s_dst);
 					//printf("%d: process %d send TRANSFER=%d to %d\n", get_physical_time(),self,msg.s_header.s_type, order.s_dst);
 				}
 				else  if (order.s_dst == self) {
-					fprintf(p->events,log_transfer_in_fmt,get_physical_time(),p->id,order.s_amount,order.s_src);
-					printf(log_transfer_in_fmt,get_physical_time(),p->id,order.s_amount,order.s_src);
+					time = get_physical_time();
+					fprintf(p->events,log_transfer_in_fmt,time,p->id,order.s_amount,order.s_src);
+					printf(log_transfer_in_fmt,time,p->id,order.s_amount,order.s_src);
 					msg.s_header = (MessageHeader) {
 						.s_magic = MESSAGE_MAGIC,
 						.s_payload_len = 0,
 						.s_type = ACK,
-						.s_local_time = get_physical_time()
+						.s_local_time = time
 					};
-					printf("TRANSFER to child: time = %d\n", msg.s_header.s_local_time);
 					set_balance(h, order.s_amount);
-					printf("TRANSFER: time = %d\n",  get_physical_time());
 					if (send(p, PARENT_ID, (const Message *)&msg) != 0){
 						perror("send ACK is failed");
 						exit(EXIT_FAILURE);
 					}
-					//fprintf(p->events,log_transfer_out_fmt,get_physical_time(),p->id,order.s_amount,order.s_dst);
-					//printf(log_transfer_out_fmt,get_physical_time(),p->id,order.s_amount,order.s_dst);
-					//printf("%d: process %d send ACK=%d to 0\n", get_physical_time(),self,msg.s_header.s_type);
 				}
 				else {
 					printf("error is occured in switch-transfer\n");
